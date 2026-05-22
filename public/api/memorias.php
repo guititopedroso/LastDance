35250778ceed1ba50656351eabb7a34999cdd627
guitito <guititopedroso@gmail.com>
2026-05-22 09:57:58 +0100

    Set JSON content type in API response

--- public/api/memorias.php
+++ public/api/memorias.php
@@ -3,6 +3,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Content-Type: application/json; charset=utf-8");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit(0);
